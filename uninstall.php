227ba71ba67953fbe3ba8f11c2e452de203b9373
amoserULC <amoser@uplync.com>
2026-03-25 14:43:00 -0400

    Update uninstall.php - swap "dpviz" for "vizier"

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,8 @@
 <?php
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 
-sql('DROP TABLE dpviz');
-sql('DROP TABLE dpviz_views');
+sql('DROP TABLE vizier');
+sql('DROP TABLE vizier_views');
 
 
 ?>
